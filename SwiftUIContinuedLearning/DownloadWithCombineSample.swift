463331d677b95e983968c11ec450fc8f79a41dc7
Rusln <cpptypxe@ya.ru>
2025-03-22 03:41:45 -0700

    [Skill] receive JSON with Combine

---
+++ SwiftUIContinuedLearning/DownloadWithCombineSample.swift
@@ -0,0 +1,18 @@
+//
+//  DownloadWithCombineSample.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by dofxmine on 22.03.2025.
+//
+
+import SwiftUI
+
+struct DownloadWithCombineSample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DownloadWithCombineSample()
+}
