fa29a88531db96e539dd872c6928603703e6b0fd
Rusln <cpptypxe@ya.ru>
2025-03-22 03:42:23 -0700

    [Skill] receive JSON with Combine

--- SwiftUIContinuedLearning/DownloadWithCombineSample.swift
+++ SwiftUIContinuedLearning/DownloadWithCombineSample.swift
@@ -6,10 +6,67 @@
 //
 
 import SwiftUI
+import Combine
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithCombineViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            //.subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .tryMap(handleOutput)
+            .decode(type: [PostModel].self, decoder: JSONDecoder())
+            .replaceError(with: [])
+            .sink(receiveValue: { [weak self] (returnedPosts) in
+                self?.posts = returnedPosts
+            })
+            
+            .store(in: &cancellables)
+
+    }
+    
+    func handleOutput(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard
+            let response = output.response as? HTTPURLResponse,
+        response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+}
 
 struct DownloadWithCombineSample: View {
+    
+    @StateObject var viewModel = DownloadWithCombineViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundStyle(.secondary)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
     }
 }
 
