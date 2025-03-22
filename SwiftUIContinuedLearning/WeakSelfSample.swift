c91d69d29cc3d894e86ff1ae6841010d0eb08310
Rusln <cpptypxe@ya.ru>
2025-03-21 20:55:14 -0700

    [skill] weak self

--- SwiftUIContinuedLearning/WeakSelfSample.swift
+++ SwiftUIContinuedLearning/WeakSelfSample.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct WeakSelfSample: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @AppStorage("count") var count: Int?
+    
+    init() {
+        count = 0
     }
+    
+    var body: some View {
+        NavigationView {
+            NavigationLink("Navigate") {
+                WeakSelfSecondScreen()
+            }
+            .navigationTitle("Screen 1")
+        }
+        .overlay(
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(.green)
+                .clipShape(Circle())
+            , alignment: .topTrailing
+        )
+    }
+}
+
+struct WeakSelfSecondScreen: View {
+    
+    @StateObject var vm = WeakSelfSecondScreenViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Second View")
+                .font(.largeTitle)
+                .foregroundStyle(.red)
+            
+            if let data = vm.data {
+                Text(data)
+            }
+        }
+    }
+}
+
+class WeakSelfSecondScreenViewModel: ObservableObject {
+    
+    @Published var data: String? = nil
+    
+    init() {
+        print("INITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 500) { [weak self] in
+            self?.data = "NEW DATA!!!"
+        }
+    }
+    
 }
 
 #Preview {
