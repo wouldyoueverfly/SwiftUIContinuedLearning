d033f28df774d13da722d16339269327733cbac3
Rusln <cpptypxe@ya.ru>
2025-03-22 02:10:02 -0700

    [Skill] Download JSON with @escaping

---
+++ SwiftUIContinuedLearning/DownloadWithEscapingSample.swift
@@ -0,0 +1,85 @@
+//
+//  DownloadWithEscapingSample.swift
+//  SwiftUIContinuedLearning
+//
+//  Created by dofxmine on 22.03.2025.
+//
+
+import SwiftUI
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+class DownloadWithEscapingViewModel: ObservableObject {
+    
+    @Published var posts: [PostModel] = []
+    
+    init() {
+        getPosts()
+    }
+    
+    func getPosts() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        downloadData(fromURL: url) { (returnedData) in
+            if let data = returnedData {
+                guard let newPosts = try? JSONDecoder().decode([PostModel].self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts = newPosts
+                }
+            } else {
+                print("NO DATA RETURNED")
+            }
+            
+        }
+    }
+    
+    func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                print("ERROR DOWNLOADING DATA")
+                completionHandler(nil)
+                return
+            }
+            print("SUCCESSFULLY DOWNLOADED DATA")
+            
+            completionHandler(data)
+            
+        }
+        .resume()
+    }
+    
+}
+
+struct DownloadWithEscapingSample: View {
+    
+    @StateObject var viewModel = DownloadWithEscapingViewModel()
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+}
+
+#Preview {
+    DownloadWithEscapingSample()
+}
